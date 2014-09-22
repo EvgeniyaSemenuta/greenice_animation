60a2457113cc4f6860de22d94cd49613fd63d6a3
Evgeniya Semenuta <EvgeniyaSemenuta@gmail.com>
2014-09-22 11:30:27 +0400

    Add third slide.

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,18 @@
 $(function() {
-    var transitives = [$(".transitive-0"), $(".transitive-1"), $(".transitive-2")];
+    var transitives = [$("#slide_2 .transitive-0"), $("#slide_2 .transitive-1"), $("#slide_2 .transitive-2"), $("#slide_2 .transitive-3"), $("#slide_2 .transitive-4"), $("#slide_2 .transitive-5")];
 
     for (var i = 0; i < transitives.length - 1; i++) {
 		(function(currentIndex, nextIndex){
-	        transitives[currentIndex].on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function() {
+	        transitives[currentIndex].on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
+	            transitives[nextIndex - 1].removeClass("transitive");
+	            transitives[nextIndex].addClass("transitive");
 	            transitives[nextIndex].removeClass("transitive-" + nextIndex);
+
+	            e.stopPropagation();
 	        });
 		})(i, i + 1)
     }
 
+    transitives[0].addClass("transitive");
     transitives[0].removeClass("transitive-0");
 });
