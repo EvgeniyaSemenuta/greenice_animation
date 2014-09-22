374a64a901bf5417282711585a499e01b48f23ff
Evgeniya Semenuta <EvgeniyaSemenuta@gmail.com>
2014-09-22 23:09:30 +0400

    Little js optimization.

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,24 @@
 $(function() {
     //var transitives = [$("#slide_3 .transitive-0"), $("#slide_3 .transitive-1"), $("#slide_3 .transitive-2"), $("#slide_3 .transitive-3"), $("#slide_3 .transitive-4")];
+    var slide = $("#slide_3");
+    
+    var transitives = slide.find("[data-transition-order]").sort(function(a, b) {
+        return a.getAttribute('data-transition-order') > b.getAttribute('data-transition-order') ? 1 : -1;
+    });
 
-    var transitivesLength = $("#slide_3 [class*=transitive-]").length;
-
-    for (var i = 0; i < transitivesLength - 1; i++) {
-        (function(currentIndex, nextIndex) {
-
-            var currentNode = $("#slide_3 .transitive-" + currentIndex);
-            var nextNode = $("#slide_3 .transitive-" + nextIndex);
-
-            currentNode.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
-                nextNode.addClass("transitive");
-                nextNode.removeClass("transitive-" + nextIndex);
-
-                e.stopPropagation();
-            });
-        })(i, i + 1)
+    var startTransition = function() {
+    	this.addClass("transitive").
+             removeClass("transitive-" + this.data("transition-order"));
     }
 
-    $("#slide_3 .transitive-0").addClass("transitive");
-    $("#slide_3 .transitive-0").removeClass("transitive-0");
+    for (var i = 0; i < transitives.length - 1; i++) {
+        (function(currentNode, nextNode) {
+            currentNode.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
+                startTransition.call(nextNode);
+                e.stopPropagation();
+            });
+        })($(transitives[i]), $(transitives[i + 1]))
+    }
+
+    startTransition.call($(transitives[0]));
 });
