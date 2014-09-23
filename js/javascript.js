3162d533029f82f5ac872417de8ac4da10534226
Evgeniya Semenuta <EvgeniyaSemenuta@gmail.com>
2014-09-23 15:31:57 +0400

    Add fifth slide.

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,24 @@
 $(function() {
-    //var transitives = [$("#slide_3 .transitive-0"), $("#slide_3 .transitive-1"), $("#slide_3 .transitive-2"), $("#slide_3 .transitive-3"), $("#slide_3 .transitive-4")];
-    var slide = $("#slide_3");
-    
-    var transitives = slide.find("[data-transition-order]").sort(function(a, b) {
-        return a.getAttribute('data-transition-order') > b.getAttribute('data-transition-order') ? 1 : -1;
-    });
 
-    var startTransition = function() {
-    	this.addClass("transitive").
-             removeClass("transitive-" + this.data("transition-order"));
-    }
+    var startTransitionForSlide = function(slide) {
+        var transitives = slide.find("[data-transition-order]").sort(function(a, b) {
+            return a.getAttribute('data-transition-order') > b.getAttribute('data-transition-order') ? 1 : -1;
+        });
+        console.log(transitives);
+        var startTransition = function() {
+            this.addClass("transitive").
+            removeClass("transitive-" + this.data("transition-order"));
+        }
 
-    for (var i = 0; i < transitives.length - 1; i++) {
-        (function(currentNode, nextNode) {
-            currentNode.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
-                startTransition.call(nextNode);
-                e.stopPropagation();
-            });
-        })($(transitives[i]), $(transitives[i + 1]))
-    }
+        for (var i = 0; i < transitives.length - 1; i++) {
+            (function(currentNode, nextNode) {
+                currentNode.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
+                    startTransition.call(nextNode);
+                    e.stopPropagation();
+                });
+            })($(transitives[i]), $(transitives[i + 1]));
+        }
 
-    startTransition.call($(transitives[0]));
+        startTransition.call($(transitives[0]));
+    }($("#slide_4"));
 });
