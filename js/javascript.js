f419605787d886f2354449eb84d029962d6eef1f
Evgeniya Semenuta <EvgeniyaSemenuta@gmail.com>
2014-09-24 22:45:25 +0400

    Add sixth and seventh slides.

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,13 @@
 $(function() {
 
+    $("#slider" ).slider({
+      min: 0,
+      max: 15,
+      slide: function( event, ui ) {
+        $( "#patients_amount" ).text( ui.value );
+      }
+    });
+
     var startTransitionForSlide = function(slide) {
         var transitives = slide.find("[data-transition-order]").sort(function(a, b) {
             return a.getAttribute('data-transition-order') > b.getAttribute('data-transition-order') ? 1 : -1;
@@ -20,5 +28,5 @@ $(function() {
         }
 
         startTransition.call($(transitives[0]));
-    }($("#slide_4"));
+    }($("#slide_6"));
 });
