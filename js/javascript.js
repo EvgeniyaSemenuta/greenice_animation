67d1b721cd321429005981a8dbea94403695b222
Evgeniya Semenuta <EvgeniyaSemenuta@gmail.com>
2014-09-25 17:57:48 +0400

    Add reset transitions for slide.

--- js/javascript.js
+++ js/javascript.js
@@ -1,49 +1,57 @@
 $(function() {
 
-    $("#slider" ).slider({
-      min: 0,
-      max: 15,
-      slide: function( event, ui ) {
-        $( "#patients_amount" ).text( ui.value );
-      }
+    $("#slider").slider({
+        min: 0,
+        max: 15,
+        slide: function(event, ui) {
+            $("#patients_amount").text(ui.value);
+        }
     });
-
-    window.location.hash = "#slide_0";
 
     var startTransitionForSlide = function(slide) {
         var transitives = slide.find("[data-transition-order]").sort(function(a, b) {
             return a.getAttribute('data-transition-order') > b.getAttribute('data-transition-order') ? 1 : -1;
         });
-        //console.log(transitives);
         var startTransition = function() {
-        	//console.log("this = ", this);
-            this.addClass("transitive").
-            removeClass("transitive-" + this.data("transition-order"));
+            this.addClass("transitive").removeClass("transitive-" + this.data("transition-order"));
         }
 
         for (var i = 0; i < transitives.length - 1; i++) {
             (function(currentNode, nextNode) {
-                currentNode.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
+                currentNode.one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
                     startTransition.call(nextNode);
                     e.stopPropagation();
                 });
             })($(transitives[i]), $(transitives[i + 1]));
         }
-        console.log("transitives[0] = ", transitives[0]);
 
-        if (transitives.length)
-        	startTransition.call($(transitives[0]));
+        setTimeout(function() {
+            if (transitives.length)
+                startTransition.call($(transitives[0]));
+        }, 0);
     }
 
-    $(".next").click(function(){
-    	var href = $(this).attr("href");
+    var resetTransitionsForSlide = function(slide) {
+        var transitives = slide.find("[data-transition-order]");
 
-    	console.log(window.location.hash);
+        transitives.each(function() {
+            var transitive = $(this);
+            transitive.addClass("transitive-" + transitive.data("transition-order")).removeClass("transitive");
+        });
+    }
 
-    	$("." + window.location.hash.substr(1)).hide();
-    	
-    	$("." + href.substr(1)).show();
+    window.location.hash = "#slide_0";
+    startTransitionForSlide($(window.location.hash));
 
-    	startTransitionForSlide($(href));
+    $(".next").click(function() {
+        var href = $(this).attr("href");
+
+        $("." + window.location.hash.substr(1)).hide();
+
+        resetTransitionsForSlide($(window.location.hash));
+
+        $("." + href.substr(1)).show();
+
+        startTransitionForSlide($(href));
     });
 });
