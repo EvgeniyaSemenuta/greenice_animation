9a74799dba9abe08c24b510a467c32f9a72a08fa
Evgeniya Semenuta <EvgeniyaSemenuta@gmail.com>
2014-09-25 13:08:16 +0400

    Add slide scrolling.

--- js/javascript.js
+++ js/javascript.js
@@ -8,12 +8,15 @@ $(function() {
       }
     });
 
+    window.location.hash = "#slide_0";
+
     var startTransitionForSlide = function(slide) {
         var transitives = slide.find("[data-transition-order]").sort(function(a, b) {
             return a.getAttribute('data-transition-order') > b.getAttribute('data-transition-order') ? 1 : -1;
         });
-        console.log(transitives);
+        //console.log(transitives);
         var startTransition = function() {
+        	//console.log("this = ", this);
             this.addClass("transitive").
             removeClass("transitive-" + this.data("transition-order"));
         }
@@ -26,7 +29,21 @@ $(function() {
                 });
             })($(transitives[i]), $(transitives[i + 1]));
         }
+        console.log("transitives[0] = ", transitives[0]);
 
-        startTransition.call($(transitives[0]));
-    }($("#slide_6"));
+        if (transitives.length)
+        	startTransition.call($(transitives[0]));
+    }
+
+    $(".next").click(function(){
+    	var href = $(this).attr("href");
+
+    	console.log(window.location.hash);
+
+    	$("." + window.location.hash.substr(1)).hide();
+    	
+    	$("." + href.substr(1)).show();
+
+    	startTransitionForSlide($(href));
+    });
 });
