74d4ef82d5e1d90ff96345954bf843717474b1ea
Evgeniya Semenuta <EvgeniyaSemenuta@gmail.com>
2014-09-26 13:13:08 +0400

    Prevent footer links break navigation.

--- js/javascript.js
+++ js/javascript.js
@@ -8,6 +8,10 @@ $(function() {
         }
     });
 
+    $("a.dead").click(function(e) {
+    	e.preventDefault();
+    });
+
     var startTransitionForSlide = function(slide) {
         var transitives = slide.find("[data-transition-order]").sort(function(a, b) {
             return a.getAttribute('data-transition-order') > b.getAttribute('data-transition-order') ? 1 : -1;
